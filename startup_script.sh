6915d5237be26d337a90fdd9c5b2c96605be7e20
aws-david <45223276+aws-david@users.noreply.github.com>
2021-04-29 08:56:49 +0200

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-firewall-cmd --zone=public --permanent --add-port=8443/tcp
-firewall-cmd --reload
+
 /usr/local/bin/send_dcvsessionready_notification.sh >/dev/null 2>&1 &
 _username="$(aws secretsmanager get-secret-value --secret-id \
                    Run_DCV_in_Batch --query SecretString  --output text | \
@@ -10,4 +9,3 @@ adduser "${_username}" -G wheel \
                    Run_DCV_in_Batch --query SecretString --output text | \
           sed 's/\"//g' | sed 's/{//' | sed 's/}//')" | chpasswd
 /bin/dcv create-session --owner "${_username}" --user "${_username}" "${_username}session"
-
